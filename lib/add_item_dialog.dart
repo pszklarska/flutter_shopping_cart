81ba32c6baa88dce6c58222eb98d1c7d9faee265
pszklarska <paulina.szklarska@droidsonroids.pl>
2018-04-24 22:04:36 +0200

    Code cleanup

--- lib/add_item_dialog.dart
+++ lib/add_item_dialog.dart
@@ -2,50 +2,65 @@ import 'package:flutter/material.dart';
 import 'package:flutter_redux/flutter_redux.dart';
 import 'package:flutter_shopping_cart/actions.dart';
 
-class AddItemDialog extends StatefulWidget {
-  @override
-  State<StatefulWidget> createState() => new AddItemDialogState();
-}
-
-class AddItemDialogState extends State<AddItemDialog> {
-  String itemName;
-
+class AddItemDialog extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return new StoreConnector<List<String>, OnAddCallback>(
         converter: (store) {
       return (itemName) => store.dispatch(AddItemAction(itemName));
     }, builder: (context, callback) {
-      return new AlertDialog(
-        contentPadding: const EdgeInsets.all(16.0),
-        content: new Row(
-          children: <Widget>[
-            new Expanded(
-              child: new TextField(
-                autofocus: true,
-                decoration: new InputDecoration(
-                    labelText: 'Item name',
-                    hintText: 'eg. Red Apples'),
-                onChanged: _handleTextChanged,
-              ),
-            )
-          ],
-        ),
-        actions: <Widget>[
-          new FlatButton(
-              child: const Text('CANCEL'),
-              onPressed: () {
-                Navigator.pop(context);
-              }),
-          new FlatButton(
-              child: const Text('ADD'),
-              onPressed: () {
-                Navigator.pop(context);
-                callback(itemName);
-              })
-        ],
-      );
+      return new AddItemDialogWidget(callback);
     });
+  }
+}
+
+class AddItemDialogWidget extends StatefulWidget {
+  final OnAddCallback callback;
+
+  AddItemDialogWidget(this.callback);
+
+  @override
+  State<StatefulWidget> createState() =>
+      new AddItemDialogWidgetState(callback);
+}
+
+class AddItemDialogWidgetState extends State<AddItemDialogWidget> {
+  String itemName;
+
+  final OnAddCallback callback;
+
+  AddItemDialogWidgetState(this.callback);
+
+  @override
+  Widget build(BuildContext context) {
+    return new AlertDialog(
+      contentPadding: const EdgeInsets.all(16.0),
+      content: new Row(
+        children: <Widget>[
+          new Expanded(
+            child: new TextField(
+              autofocus: true,
+              decoration: new InputDecoration(
+                  labelText: 'Item name', hintText: 'eg. Red Apples'),
+              onChanged: _handleTextChanged,
+            ),
+          )
+        ],
+      ),
+      actions: <Widget>[
+        new FlatButton(
+            child: const Text('CANCEL'),
+            onPressed: () {
+              Navigator.pop(context);
+            }),
+        new FlatButton(
+            child: const Text('ADD'),
+            onPressed: () {
+              Navigator.pop(context);
+              callback(itemName);
+            })
+      ],
+    );
   }
 
   _handleTextChanged(String newItemName) {
