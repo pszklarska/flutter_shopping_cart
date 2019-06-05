e8f480ca347a91448861b49f5cdf793932ec2b34
pszklarska <pszklarska@gmail.com>
2019-06-05 11:41:53 +0200

    Adding viewModel to shopping list widget

--- lib/list/shopping_list.dart
+++ lib/list/shopping_list.dart
@@ -3,18 +3,32 @@ import 'package:flutter_redux/flutter_redux.dart';
 import 'package:flutter_shopping_cart/list/shopping_list_item.dart';
 import 'package:flutter_shopping_cart/model/app_state.dart';
 import 'package:flutter_shopping_cart/model/cart_item.dart';
+import 'package:redux/redux.dart';
 
 class ShoppingList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return StoreConnector<AppState, List<CartItem>>(
-      converter: (store) => store.state.cartItems,
-      builder: (context, list) {
+    return StoreConnector<AppState, ShoppingListViewModel>(
+      converter: (store) => ShoppingListViewModel.build(store),
+      builder: (context, viewModel) {
         return ListView.builder(
-          itemCount: list.length,
-          itemBuilder: (context, position) => ShoppingListItem(list[position]),
+          itemCount: viewModel.cartItems.length,
+          itemBuilder: (context, position) =>
+              ShoppingListItem(viewModel.cartItems[position]),
         );
       },
+    );
+  }
+}
+
+class ShoppingListViewModel {
+  final List<CartItem> cartItems;
+
+  ShoppingListViewModel({this.cartItems});
+
+  static ShoppingListViewModel build(Store<AppState> store) {
+    return ShoppingListViewModel(
+      cartItems: store.state.cartItems,
     );
   }
 }
--- lib/redux/store.dart
+++ lib/redux/store.dart
@@ -1,21 +1,16 @@
-import 'package:flutter_shopping_cart/data/api_client.dart';
 import 'package:flutter_shopping_cart/model/app_state.dart';
 import 'package:flutter_shopping_cart/redux/prefs_middleware.dart';
 import 'package:redux/redux.dart';
 import 'package:redux_dev_tools/redux_dev_tools.dart';
 
-import 'api_middleware.dart';
 import 'reducers.dart';
 
 Store<AppState> createReduxStore() {
-  final apiClient = ApiClient();
-
   return DevToolsStore<AppState>(
     appStateReducers,
     initialState: AppState.empty,
     middleware: [
       PrefsMiddleware(),
-      ApiMiddleware(apiClient),
     ],
   );
 }
